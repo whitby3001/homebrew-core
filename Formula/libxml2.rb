9cb7c50f7cb86d083fccd0c5f394b89d9476d3a4
David Whitby <david.whitby@freeagent.com>
2021-03-23 21:45:48 +0000

    Update libxml2.rb

--- Formula/libxml2.rb
+++ Formula/libxml2.rb
@@ -6,6 +6,7 @@ class Libxml2 < Formula
     sha256 "aafee193ffb8fe0c82d4afef6ef91972cbaf5feea100edc2f262750611b4be1f"
   
     bottle do
+        root_url "https://bintray.com/homebrew/bottles/libxml2"
        cellar :any
       sha256 "2a7de29b64f7bd74990ff1fd1b00d52333e587ae567e78bbab811a33b91141d6" => :catalina
       sha256 "1e6143e9bfb756fe80e4a1db417b722569429a815365ed9070556e81bd2db02a" => :mojave
